081160a96a021775c1900e425cc063ec7bbed7b9
Gustavo Cotta <gmcotta34@gmail.com>
2020-04-22 22:56:13 -0300

    Header and Import Page completed

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,30 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
+    data.append('file', uploadedFiles[0].file);
 
     // TODO
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
+      setUploadedFiles([]);
+      history.push('/');
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    files.map(file =>
+      setUploadedFiles([
+        {
+          file,
+          name: file.name,
+          readableSize: filesize(file.size),
+        },
+      ]),
+    );
   }
 
   return (
